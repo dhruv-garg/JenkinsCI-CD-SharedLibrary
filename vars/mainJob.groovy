a7661d22b4ca16aae1177bd6e8b4d1853aac5056
dhruv-garg <dhruvgarg074@gmail.com>
2023-03-28 12:31:37 +0530

    Pushing new library

---
+++ vars/mainJob.groovy
@@ -0,0 +1,31 @@
+def call() {
+	pipeline {
+		agent any
+		environment {
+			IMAGE_REPO_NAME="demo-repository"
+			REPOSITORY_URI = "dhruv-garg/${IMAGE_REPO_NAME}"
+			DOCKERHUB_CREDENTIALS=credentials('dockerhub')
+		}
+		stages {
+			stage('Building image') {
+				steps {
+					sh 'docker build -t $REPOSITORY_URI .'
+				}
+			}
+			stage('Loggin to Dockerhub') {
+				steps {
+					sh 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
+				}
+			}
+			stage("pushing to docker hub') {
+				steps {
+					sh 'docker push $REPOSITORY_URI'
+				}
+			}
+		post {
+			always {
+				sh 'docker logout'
+			}
+		}
+	}
+}
